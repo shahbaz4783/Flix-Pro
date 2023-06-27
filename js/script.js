57b3a8a01b83c9eb0793a948b4c62fbddca4059c
shahbaz4783 <shahbaz4783@gmail.com>
2023-06-27 17:27:22 +0530

    ui changes and display popular, upcoming and top rated movies

--- js/script.js
+++ js/script.js
@@ -2,41 +2,12 @@ const global = {
 	currentPage: window.location.pathname,
 };
 
-// display popular movies
-const displayPopularMovies = async () => {
-    const { results } = await fetchAPIdata('movie/popular');
-    let movieCount = 0; // Variable to track the number of displayed movies
-  
-    results.forEach((movie) => {
-      if (movieCount < 10) {
-        const cardContainer = document.createElement('div');
-        cardContainer.classList.add('card-container');
-        cardContainer.innerHTML = `
-          <div class="card">
-            <div class="front-content">
-              <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
-            </div>
-            <div class="content">
-              <p class="heading">${movie.title}</p>
-              <h3>Released: ${movie.release_date}</h3>
-              <p class="description">
-                ${movie.overview}
-              </p>
-              <button>Show Details</button>
-            </div>
-          </div>
-        `;
-        document.querySelector('.top-rated-list').append(cardContainer);
-        movieCount++;
-      }
-    });
-  };
-  
 
 // Fetch Data From TMBD API
 const fetchAPIdata = async (endpoint) => {
 	const API_KEY = '47152db3059591a245fa638f38ce9f76';
 	const API_URL = 'https://api.themoviedb.org/3/';
+    
 
 	const resonse = await fetch(
 		`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
@@ -46,6 +17,112 @@ const fetchAPIdata = async (endpoint) => {
 	return data;
 };
 
+
+// display now playing movies
+const displayNowPlayingMovies = async () => {
+    let movieList = []; // Array to store the "Now Playing" movies
+    let movieIndex = 0; // Index to track the current movie being displayed
+    let intervalId; // Variable to store the interval ID
+  
+    const fetchMovies = async () => {
+      const { results } = await fetchAPIdata('movie/now_playing');
+      movieList = results;
+    };
+  
+    const showNextMovie = () => {
+      const movie = movieList[movieIndex];
+  
+      const releaseDate = new Date(movie.release_date);
+      const formattedDate = `${releaseDate.getDate()} ${releaseDate.toLocaleString('default', { month: 'short' })} ${releaseDate.getFullYear()}`;
+  
+      const cardContainer = document.createElement('div');
+      cardContainer.classList.add('playing-movie-card');
+      cardContainer.innerHTML = `
+        <img class="playing-poster" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+        <div class="playing-movie-content">
+          <span class="movie-title">${movie.title}</span>
+          <p class="playing-movie-description">Released: ${formattedDate}</p>
+          <button class="details">Details</button>
+        </div>
+      `;
+  
+      const nowPlayingList = document.querySelector('.now-playing-list');
+      nowPlayingList.innerHTML = ''; // Clear previous movie
+      nowPlayingList.append(cardContainer);
+  
+      movieIndex = (movieIndex + 1) % movieList.length; // Move to the next movie
+  
+      // Clear previous interval and set new interval to show the next movie after 5 seconds
+      clearInterval(intervalId);
+      intervalId = setInterval(showNextMovie, 3000);
+    };
+  
+    await fetchMovies(); // Fetch the initial list of "Now Playing" movies
+    showNextMovie(); // Show the first movie immediately
+  
+    // Set interval to show the next movie every 5 seconds
+    intervalId = setInterval(showNextMovie, 3000);
+  };
+  
+
+
+
+// display popular movies
+const displayPopularMovies = async () => {
+    const { results } = await fetchAPIdata('movie/top_rated');
+    let movieCount = 0;
+  
+    results.forEach((movie) => {
+    if (movieCount < 14) {
+
+        const releaseDate = new Date(movie.release_date);
+        const formattedDate = `${releaseDate.getDate()} ${releaseDate.toLocaleString('default', { month: 'short' })} ${releaseDate.getFullYear()}`;
+  
+        const cardContainer = document.createElement('div');
+        cardContainer.classList.add('movie-card');
+        cardContainer.innerHTML = `
+        <img class="poster" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+        <div class="movie-content">
+            <span class="movie-title">${movie.title}</span>
+            <p class="movie-description">Released: ${formattedDate}</p>
+            <button class="details">Details</button>
+        </div>
+        `;
+        document.querySelector('.top-rated-list').append(cardContainer);
+        movieCount++;
+      }
+    });
+  };
+
+  // display upcoming movies
+  const displayUpcomingMovies = async () => {
+    const { results } = await fetchAPIdata('movie/upcoming');
+    let movieCount = 0;
+  
+    results.forEach((movie) => {
+    if (movieCount < 7) {
+
+        const releaseDate = new Date(movie.release_date);
+        const formattedDate = `${releaseDate.getDate()} ${releaseDate.toLocaleString('default', { month: 'short' })} ${releaseDate.getFullYear()}`;
+  
+        const cardContainer = document.createElement('div');
+        cardContainer.classList.add('movie-card');
+        cardContainer.innerHTML = `
+        <img class="poster" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+        <div class="movie-content">
+            <span class="movie-title">${movie.title}</span>
+            <p class="movie-description">Released: ${formattedDate}</p>
+            <button class="details">Details</button>
+        </div>
+        `;
+        document.querySelector('.upcoming-list').append(cardContainer);
+        movieCount++;
+      }
+    });
+  };
+
+
+
 // Init App
 const init = () => {
 	switch (global.currentPage) {
@@ -53,6 +130,8 @@ const init = () => {
 		case '/index.html':
 			console.log('Home');
 			displayPopularMovies();
+            displayUpcomingMovies();
+            displayNowPlayingMovies();
 			break;
 		case '/shows.html':
 			console.log('Shows');
